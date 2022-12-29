f08c7731e62270da86d895ab5f0b25602a6d5bf7
syw1114 <tkdvlftj11@naver.com>
2022-12-29 12:46:00 +0900

    12/29 script 기본 ( back, href)

---
+++ javascript/js/common.js
@@ -0,0 +1,11 @@
+const helloworld = () => {
+    alert('Hello world');
+}
+
+const moveExample02 = () =>{
+    location.replace('./example02.html');
+    // window.location.href('./example02.html');
+}
+const back = () =>{
+    history.back();
+}
